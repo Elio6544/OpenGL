0143f0651275c8f417d81e69eac52b7fc111e917
Max Tsvirko <scvirko4623@gmail.com>
2025-01-01 15:30:19 +0200

    Add files via upload

---
+++ Practical work 2/Lab 2/DrawFigures.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using static Lab_2.OpenGL;
+
+namespace Lab_2
+{
+    public class DrawFigures
+    {
+        private WindowSize windowSize;
+        private Random random = new Random();
+
+        public float sideFigure { get; }
+        public int horCount { set; get; }
+        public int verCount { set; get; }
+        public uint polygonMode { set; get; }
+        public uint shadeModel { set; get; }
+
+        public DrawFigures(WindowSize windowSize)
+        {
+            this.windowSize = windowSize;
+
+            sideFigure = 10f;
+
+            horCount = 1;
+            verCount = 1;
+
+            polygonMode = GL_FILL;
+            shadeModel = GL_SMOOTH;
+        }
+
+        public void Draw()
+        {
+            for(int ver = 0; ver < verCount; ver++)
+            {
+                for (int hor = 0; hor < horCount; hor++)
+                {
+                    DFigures(hor, ver);
+                }
+            }
+        }
+
+        public void SetWindowSize(WindowSize windowSize)
+        {
+            this.windowSize = windowSize;
+        }
+
+        private double RandomColor() => 
+            random.NextDouble();
+
+        private void DFigures(int hor, int ver)
+        {
+            float zero_x = (1.5f - (1.25f * (horCount - 1))) * sideFigure;
+            float zero_y = (verCount - 1) * (sideFigure * MathF.Sqrt(3)) / 2;
+
+            zero_y = hor % 2 != 0 ? zero_y - (sideFigure * MathF.Sqrt(3)) / 2 : zero_y;
+
+            float coef_x = hor * (sideFigure * 2.5f);
+            float coef_y = ver * (sideFigure * MathF.Sqrt(3));
+
+            float pos_x = zero_x + coef_x;
+            float pos_y = zero_y - coef_y;
+
+            glPolygonMode(GL_FRONT_AND_BACK, polygonMode);
+            glShadeModel(shadeModel);
+
+            glPointSize(5);
+            glLineWidth(5);
+
+            // figure 1
+            glBegin(GL_POLYGON);
+                glColor3d(0, 1, 0);
+                glVertex2d(pos_x - sideFigure, pos_y);
+                glVertex2d(pos_x - (3 * sideFigure), pos_y);
+                glVertex2d(pos_x - (2.5 * sideFigure), pos_y + ((sideFigure * Math.Sqrt(3)) / 2));
+                glColor3d(1, 0, 0);
+                glVertex2d(pos_x - (1.5 * sideFigure), pos_y + ((sideFigure * Math.Sqrt(3)) / 2));
+            glEnd();
+
+            // figure 2
+            glBegin(GL_POLYGON);
+                glColor3d(1, 0, 0);
+                glVertex2d(pos_x - sideFigure, pos_y);
+                glVertex2d(pos_x - (1.5 * sideFigure), pos_y + ((sideFigure * Math.Sqrt(3)) / 2));
+                glColor3d(0, 1, 0);
+                glVertex2d(pos_x - (0.5 * sideFigure), pos_y + ((sideFigure * Math.Sqrt(3)) / 2));
+            glEnd();
+
+            // figure 3
+            glBegin(GL_POLYGON);
+                glColor3d(0, 1, 0);
+                glVertex2d(pos_x, pos_y);
+                glVertex2d(pos_x - (0.5 * sideFigure), pos_y + ((sideFigure * Math.Sqrt(3)) / 2));
+                glColor3d(1, 1, 0); 
+                glVertex2d(pos_x - sideFigure, pos_y);
+            glEnd();
+
+            // figure 4
+            glBegin(GL_POLYGON);
+                glColor3d(1, 1, 0);
+                glVertex2d(pos_x - sideFigure, pos_y);
+                glVertex2d(pos_x - (3 * sideFigure), pos_y);
+                glVertex2d(pos_x - (2.5 * sideFigure), pos_y - ((sideFigure * Math.Sqrt(3)) / 2));
+                glColor3d(1, 0, 0);
+                glVertex2d(pos_x - (1.5 * sideFigure), pos_y - ((sideFigure * Math.Sqrt(3)) / 2));
+            glEnd();
+
+            // figure 5
+            glBegin(GL_POLYGON);
+                glColor3d(1, 0, 0);
+                glVertex2d(pos_x - sideFigure, pos_y);
+                glVertex2d(pos_x - (1.5 * sideFigure), pos_y - ((sideFigure * Math.Sqrt(3)) / 2));
+                glColor3d(1, 1, 0);
+                glVertex2d(pos_x - (0.5 * sideFigure), pos_y - ((sideFigure * Math.Sqrt(3)) / 2));
+            glEnd();
+
+            // figure 6
+            glBegin(GL_POLYGON);
+                glColor3d(1, 1, 0);
+                glVertex2d(pos_x, pos_y);
+                glVertex2d(pos_x - (0.5 * sideFigure), pos_y - ((sideFigure * Math.Sqrt(3)) / 2));
+                glColor3d(0, 1, 0);
+                glVertex2d(pos_x - sideFigure, pos_y);
+            glEnd();
+        }
+    }
+}
---
+++ Practical work 2/Lab 2/RenderControl/RenderControl.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Lab_2
+{
+    public partial class RenderControl : OpenGL
+    {
+        private WindowSize windowSize;
+        private float sideWindow;
+
+        public DrawFigures figures { get; set; }
+        
+        public RenderControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OnStart(object sender, EventArgs e)
+        {
+            sideWindow = 20f;
+            windowSize = new WindowSize(-sideWindow, sideWindow, -sideWindow, sideWindow);
+
+            figures = new DrawFigures(windowSize);
+        }
+
+        private void OnRender(object sender, EventArgs e)
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glLoadIdentity();
+
+            // Устанавливаем квадратную область просмотра, сохраняя пропорции
+            if (Width > Height)
+            {
+                // Если ширина больше высоты, центрируем по горизонтали
+                glViewport((Width - Height) / 2, 0, Height, Height);
+            }
+            else
+            {
+                // Если высота больше ширины, центрируем по вертикали
+                glViewport(0, (Height - Width) / 2, Width, Width);
+            }
+
+            glOrtho(windowSize.Xmin, windowSize.Xmax, windowSize.Ymin, windowSize.Ymax, -1, 1);
+
+            figures.Draw();
+        }
+
+        public void UpdateSideWindow()
+        {
+            float _hor = figures.horCount * figures.sideFigure * 2.5f;
+            float _ver = figures.verCount * (figures.sideFigure * MathF.Sqrt(3));
+
+            if (_hor > _ver)
+            {
+                sideWindow = _hor / 2 + 5;
+            }
+            else
+            {
+                sideWindow = _ver / 2 + 10 + (figures.sideFigure * MathF.Sqrt(3)) / 2;
+            }
+
+            windowSize = new WindowSize(-sideWindow, sideWindow, -sideWindow, sideWindow);
+            figures.SetWindowSize(windowSize);
+
+            Invalidate();
+        }
+    }
+}
+
