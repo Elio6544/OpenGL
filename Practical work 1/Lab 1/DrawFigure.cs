e3023e259caf64ea5e002b561f3baa83650f3942
Max Tsvirko <scvirko4623@gmail.com>
2025-01-01 15:27:25 +0200

    Add files via upload

---
+++ Practical work 1/Lab 1/DrawFigure.cs
@@ -0,0 +1,69 @@
+﻿using static Lab_1.OpenGL;
+
+namespace Lab_1
+{
+    internal class DrawFigure
+    {
+        private float Xmin, Xmax, Ymin, Ymax;
+
+        private Vertex[] VertexesFigure;
+        private float merge;
+
+        public DrawFigure(float Xmin, float Xmax, float Ymin, float Ymax)
+        {
+            this.Xmin = Xmin;
+            this.Xmax = Xmax;
+            this.Ymin = Ymin;
+            this.Ymax = Ymax;
+
+            VertexesFigure = new Vertex[] {
+                new Vertex(Xmin, Ymin),
+                new Vertex(Xmin + 1f, Ymin),
+                new Vertex(Xmin + 2f, Ymin + 1f),
+                new Vertex(Xmin + 3f, Ymin + 3f),
+                new Vertex(Xmin + 2f, Ymin + 4f),
+                new Vertex(Xmin, Ymin + 3f),
+                };
+
+            merge = 6f;
+        }
+
+        public void Draw()
+        {
+            glColor3d(238f / 255f, 223f / 255f, 122f / 255f);
+
+            DFigure();
+            DPorintsFigure();
+
+            glColor3d(0, 0, 0);
+        }
+
+        private void DFigure()
+        {
+            glLineWidth(8);
+            glBegin(GL_LINE_LOOP);
+                glVertex2d(VertexesFigure[0].x, VertexesFigure[0].y);
+                glVertex2d(VertexesFigure[1].x, VertexesFigure[1].y);
+                glVertex2d(VertexesFigure[2].x, VertexesFigure[2].y);
+                glVertex2d(VertexesFigure[3].x, VertexesFigure[3].y);
+                glVertex2d(VertexesFigure[4].x, VertexesFigure[4].y);
+                glVertex2d(VertexesFigure[5].x, VertexesFigure[5].y);
+            glEnd();
+            glLineWidth(1);
+        }
+
+        private void DPorintsFigure()
+        {
+            glPointSize(16);
+            glBegin(GL_POINTS);
+                glVertex2d(VertexesFigure[0].x + merge, VertexesFigure[0].y);
+                glVertex2d(VertexesFigure[1].x + merge, VertexesFigure[1].y);
+                glVertex2d(VertexesFigure[2].x + merge, VertexesFigure[2].y);
+                glVertex2d(VertexesFigure[3].x + merge, VertexesFigure[3].y);
+                glVertex2d(VertexesFigure[4].x + merge, VertexesFigure[4].y);
+                glVertex2d(VertexesFigure[5].x + merge, VertexesFigure[5].y);
+            glEnd();
+            glPointSize(1);
+        }
+    }
+}
---
+++ Practical work 1/Lab 1/RenderControl/RenderControl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lab_1
+{
+    public partial class RenderControl : OpenGL
+    {
+        private float Xmin, Xmax;
+        private float Ymin, Ymax;
+
+        private DrawGrid _grid;
+        private DrawFigure _figure;
+        
+        public RenderControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            Xmin = -8.5f;
+            Xmax = 0.5f;
+            Ymin = -3.5f;
+            Ymax = 0.5f;
+
+            _grid = new DrawGrid(Xmin, Xmax, Ymin, Ymax);
+            _figure = new DrawFigure(Xmin, Xmax, Ymin, Ymax);
+        }
+
+        private void OnRender(object sender, EventArgs e)
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glLoadIdentity();
+
+            glViewport(0, 0, Width, Height);
+
+            glOrtho(Xmin - 2, Xmax + 2, Ymin - 2, Ymax + 2, -1, 1);
+
+            _grid.Draw();
+
+            glColor3d(216f / 255f, 162f / 255f, 94f / 255f);
+            DrawText("X1", Xmin, Ymin - 1.5);
+            DrawText("X2", Xmax, Ymin - 1.5);
+
+            DrawText("Y1", Xmin - 1.5, Ymin);
+            DrawText("Y2", Xmin - 1.5, Ymax);
+            glColor3d(0, 0, 0);
+
+            _figure.Draw();
+        }
+    }
+}
+
